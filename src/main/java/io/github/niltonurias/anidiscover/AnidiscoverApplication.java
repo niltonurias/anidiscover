70a831ba325b48812ea05751afff95f9510a9b43
Nilton Urias da Cruz Junior <vte_ncruz@uolinc.com>
2021-11-25 22:43:36 -0300

    first commit

---
+++ src/main/java/io/github/niltonurias/anidiscover/AnidiscoverApplication.java
@@ -0,0 +1,13 @@
+package io.github.niltonurias.anidiscover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnidiscoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnidiscoverApplication.class, args);
+	}
+
+}
